aab3590b9371718f3b9c2bbe411adc4a567ee610
Jared Burrows <jaredsburrows@gmail.com>
2015-05-15 22:59:43 -0400

    Add Robolectric fix

--- app/src/test/java/burrows/apps/math/activity/MainActivityTest.java
+++ app/src/test/java/burrows/apps/math/activity/MainActivityTest.java
@@ -1,14 +1,10 @@
 package burrows.apps.math.activity;
 
-import android.os.Build.VERSION_CODES;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricGradleTestRunner;
-import org.robolectric.annotation.Config;
 
-import burrows.apps.math.BuildConfig;
 import burrows.apps.math.TestBase;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -21,7 +17,6 @@ import static org.robolectric.Robolectric.setupActivity;
  * @since 0.0.1
  */
 @RunWith(RobolectricGradleTestRunner.class)
-@Config(constants = BuildConfig.class, emulateSdk = VERSION_CODES.LOLLIPOP)
 public class MainActivityTest extends TestBase {
 
     MainActivity mainActivity;
@@ -35,6 +30,6 @@ public class MainActivityTest extends TestBase {
 
     @Test
     public void test_example() {
-        assertThat("test", not(nullValue()));
+        assertThat(mainActivity, not(nullValue()));
     }
 }
